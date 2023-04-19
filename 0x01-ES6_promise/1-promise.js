b3a558befe7f873c2cb4e57187c5ec85a1c3edf6
Sanctus-Peter <106772343+Sanctus-Peter@users.noreply.github.com>
2023-04-19 17:46:12 +0100

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,18 +1,3 @@
-/*
-  Creates a Promise that represents a prototype API call with a given boolean parameter.
- 
-  @returns {Promise} A Promise that resolves with an object containing status and body properties if the API call is successful,
-                     and rejects with an error object if the API call fails.
-  @example
-  fakeAPICall(true)
-    .then(response => {
-      console.log('API call successful:', response.status, response.body);
-    })
-    .catch(error => {
-      console.error('API call failed:', error.message);
-    });
-  @param param
- */
 export default function (param) {
   return new Promise((resolve, reject) => {
     const object = {
