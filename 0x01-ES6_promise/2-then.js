e599f9725fc6de299b842252cb2cfd9198704d05
Sanctus-Peter <106772343+Sanctus-Peter@users.noreply.github.com>
2023-04-19 17:57:41 +0100

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,20 +1,3 @@
-/*
- * Handles the response from an API call represented as a Promise.
- *
- * @param {Promise} promise - A Promise that represents the response from an API call.
- * @returns {Promise} A Promise that resolves with an object containing status and body properties if the API call is successful,
- *                    and rejects with an error object if the API call fails. The Promise will always call the final block
- *                    to log a message indicating that a response was received from the API.
- * @example
- * const apiPromise = fetchDataFromAPI(); // Fetch data from API and return a Promise
- * handleResponseFromAPI(apiPromise)
- *   .then(response => {
- *     console.log('API call successful:', response.status, response.body);
- *   })
- *   .catch(error => {
- *     console.error('API call failed:', error.message);
- *   });
- */
 export default function handleResponseFromAPI(promise) {
   return promise
       .then(() => ({ status: 200, body: 'success' }))
